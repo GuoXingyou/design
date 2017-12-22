8da47427b755ee5648585763598511da067a4606
GuoXingyou <635244720@qq.com>
2017-12-22 15:37:13 +0800

    test for java8 ，about streamAPI and lambda

---
+++ src/main/java/com/jdk8/SimpleInterface.java
@@ -0,0 +1,18 @@
+package com.jdk8;
+
+/**
+ * @Author: Jax
+ * @Email: guoxingyou@xjiye.com
+ * @Date: 2017/12/22/15:28
+ * @Desc:
+ **/
+public interface SimpleInterface {
+
+    void doSomeThings();
+
+    default void doOtherThings(){
+        System.out.println("Interface do other things.");
+    }
+
+
+}
---
+++ src/main/java/com/jdk8/SimpleInterfaceImpl.java
@@ -0,0 +1,24 @@
+package com.jdk8;
+
+/**
+ * @Author: Jax
+ * @Email: guoxingyou@xjiye.com
+ * @Date: 2017/12/22/15:30
+ * @Desc:
+ **/
+public class SimpleInterfaceImpl implements SimpleInterface{
+
+    /**
+     * 如果实现了多个接口，且不同接口中有相同default方法，jvm会抛出编译异常
+     */
+
+    @Override
+    public void doSomeThings() {
+        System.out.println("Class do some things.");
+    }
+
+    public static void main(String[] args) {
+        SimpleInterface simpleInterface = new SimpleInterfaceImpl();
+        simpleInterface.doSomeThings();
+    }
+}
